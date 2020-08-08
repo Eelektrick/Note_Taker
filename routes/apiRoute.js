4aae2f143d2f4296177009810ae858f70ee6577a
Corey Post <corey.post0120@gmail.com>
2020-08-07 22:48:50 -0600

    altered app.post function

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -11,25 +11,18 @@ module.exports = function(app){
     app.get("/api/notes", (req, res) => {
         res.json(dbFile);
     });
+
+    app.post("/api/notes", function(req, res){
+        dbFile =JSON.parse(fs.readFileSync(jsonRoute, "utf8"));
+
+        const randomId = () => parseInt(100 * Math.random());
+        req.body.id = randomId();
+        dbFile.push(req.body);
+        let notes = JSON.stringify(dbFile);
+        savedbFile(notes);
+        res.json(notes);
+    });
 }
-
-// async function returnData(){
-//     const notes = await readFileAsync(jsonRoute, "utf8");
-
-//     let parsedArray = JSON.parse(notes);
-//     return(parsedArray);
-// };
-
-// app.post("/api/notes", async function(req, res){
-//     let notes = await returnData();
-
-//     console.log(req.body);
-//     notes.push(req.body);
-//     stringedArray = JSON.stringify(notes);
-
-//     const post = await writeFileAsync(jsonRoute, stringedArray, "utf8");
-//     res.join(stringedArray);
-// });
 
 // app.delete("/api/notes/:id", (req, res) => {
 //     let deletedNotes = JSON.parse(fs.readFileSync(jsonRoute, "utf8"));
